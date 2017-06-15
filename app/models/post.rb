c2652244eaac12bc3bd29273a55e261ae6ef1e94
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-06-15 14:00:35 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,7 +11,7 @@ class Post < ActiveRecord::Base
   private
 
   def is_title_case
-    if title.split.any?{|w|w[0].upcase != w[0]}
+    if title.split.any?{ |w| w[0].upcase != w[0] } 
       errors.add(:title, "Title must be in title case")
     end
   end
